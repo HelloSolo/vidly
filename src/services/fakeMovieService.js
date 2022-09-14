53bd3866a659346c5384ecd14d4c2637f102d03f
HelloSolo <davidalimi148@gmail.com>
2022-09-14 21:26:43 +0100

    fixed genre bug

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -79,7 +79,7 @@ export function getMovie(id) {
 export function saveMovie(movie) {
    let movieInDb = movies.find((m) => m._id === movie._id) || {};
    movieInDb.title = movie.title;
-   movieInDb.genre = genresAPI.genres.find((g) => g._id === movie.genre.id);
+   movieInDb.genre = genresAPI.genres.find((g) => g._id === movie.genre._id);
    movieInDb.numberInStock = movie.numberInStock;
    movieInDb.dailyRentalRate = movie.dailyRentalRate;
 
