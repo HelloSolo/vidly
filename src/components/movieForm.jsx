6e2b87b96b52ba49cbf89d2a54a188718ccf0ebe
HelloSolo <davidalimi148@gmail.com>
2022-09-14 18:59:47 +0100

    added movie form without genre

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
 
-const MovieForm = ({ match, history }) => {
-   return (
-      <div>
-         <h1>Movie Form {match.params._id}</h1>
-         <button
-            className="btn btn-primary btn-sm"
-            onClick={() => history.push("/movies")}
-         >
-            Save
-         </button>
-      </div>
-   );
-};
+class MovieForm extends Form {
+   state = {
+      data: { title: "", numberInStock: "", rate: "" },
+      errors: {},
+   };
+
+   validationRules = {
+      title: Joi.string().required().label("Title"),
+      numberInStock: Joi.number()
+         .min(0)
+         .max(100)
+         .required()
+         .label("Number in Stock"),
+      rate: Joi.number().min(0).max(10).required().label("Rate"),
+   };
+
+   schema = Joi.object(this.validationRules);
+
+   doSubmit = () => {
+      this.props.history.push("/movies");
+   };
+
+   render() {
+      return (
+         <div>
+            <h1>Movie Form</h1>
+            <form onSubmit={this.handleSubmit}>
+               {this.renderInput("title", "Title")}
+               {/* {this.renderInput("genre", "Genre", "select")} */}
+               {this.renderInput("numberInStock", "Number in Stock", "number")}
+               {this.renderInput("rate", "Rate")}
+               {this.renderButton("Save")}
+            </form>
+         </div>
+      );
+   }
+}
 
 export default MovieForm;
