039520de6899ba97e75dba97324c086f7323c362
HelloSolo <davidalimi148@gmail.com>
2022-09-19 13:34:52 +0100

    logging in users upon registration

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -19,7 +19,9 @@ class RegisterForm extends Form {
 
    doSubmit = async () => {
       try {
-         await register(this.state.data);
+         const response = await register(this.state.data);
+         localStorage.setItem("token", response.headers["x-auth-token"]);
+         this.props.history.push("/");
       } catch (error) {
          if (error.response && error.response.status === 400) {
             const errors = { ...this.state.errors };
