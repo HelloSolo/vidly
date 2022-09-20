7a0003555ea4e3f4dbf78f9765cc8f41978ac76a
HelloSolo <davidalimi148@gmail.com>
2022-09-20 09:20:13 +0100

    added admin activities

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import authService from "../services/authService";
+import auth from "../services/authService";
 import Like from "./common/like";
 import Table from "./common/table";
 
 class MoviesTable extends Component {
+   deleteColumn = {
+      key: "delete",
+      content: (movie) => (
+         <button
+            className="btn btn-danger btn-sm"
+            onClick={() => this.props.onDelete(movie)}
+         >
+            Delete
+         </button>
+      ),
+   };
+
+   constructor() {
+      super();
+      const user = auth.getCurrentUser();
+      if (user && user.isAdmin) {
+         this.columns.push(this.deleteColumn);
+      }
+   }
    columns = [
       {
          path: "title",
@@ -24,17 +45,6 @@ class MoviesTable extends Component {
                liked={movie.liked}
                onClick={() => this.props.onLike(movie)}
             />
-         ),
-      },
-      {
-         key: "delete",
-         content: (movie) => (
-            <button
-               className="btn btn-danger btn-sm"
-               onClick={() => this.props.onDelete(movie)}
-            >
-               Delete
-            </button>
          ),
       },
    ];
