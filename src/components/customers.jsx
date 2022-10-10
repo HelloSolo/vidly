b63de27ace43d44fa384f0a9b2720bd871b818d3
HelloSolo <davidalimi148@gmail.com>
2022-10-10 10:36:12 +0100

    created customer component

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,11 +1,39 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
+import { getUser } from "../services/userService";
+import { getCustomer, updateCustomer } from "../services/customerService";
+import { toast } from "react-toastify";
 import { setStyle } from "./utils/setStyle";
 
-const Customers = () => {
-   useEffect(() => {
-      setStyle();
-   });
-   return <h1>Customers</h1>;
-};
+class Customer extends Component {
+   state = {
+      user: {},
+      customer: {},
+   };
 
-export default Customers;
+   async popuplateUser() {
+      const { data: user } = await getUser();
+      this.setState({ user });
+   }
+
+   async popuplateCustomer() {
+      const { data: customer } = await getCustomer();
+      this.setState({ customer });
+   }
+
+   async componentDidMount() {
+      setStyle();
+      await this.popuplateUser();
+      await this.popuplateCustomer();
+   }
+
+   render() {
+      const { user, customer } = this.state;
+      return (
+         <div>
+            <h1>Movie Form</h1>
+         </div>
+      );
+   }
+}
+
+export default Customer;
