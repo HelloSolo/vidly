f32151b80d806bf762866ce504c7f515eba212b7
HelloSolo <davidalimi148@gmail.com>
2022-12-22 10:14:23 +0100

    updated bug in navBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
 
 const NavBar = ({ user, onClickSearch, onClickHome }) => {
+   const [navBar, setNavBar] = useState("collapse navbar-collapse");
+
+   useEffect(() => {
+      setNavBar("collapse navbar-collapse");
+   });
+
+   const onClickLink = (func) => {
+      let nav = document.getElementById("navbarNavAltMarkup");
+      setNavBar(nav.className);
+      if (func) {
+         func();
+      }
+      return;
+   };
+
    return (
       <nav className="navbar navbar-expand-lg navbar--custom">
          <div className="container-fluid">
@@ -25,32 +40,34 @@ const NavBar = ({ user, onClickSearch, onClickHome }) => {
                      aria-hidden="true"></i>
                </span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className={navBar} id="navbarNavAltMarkup">
                <div className="navbar-nav">
                   <NavLink
                      className="nav-link nav-link--custom"
                      aria-current="page"
-                     to="/movies"
-                     onClick={() => onClickHome()}>
+                     to={"/movies"}
+                     onClick={() => onClickLink()}>
                      Movies
                   </NavLink>
                   <NavLink
                      className="nav-link nav-link--custom"
                      to="/movies"
-                     onClick={() => onClickSearch()}>
+                     onClick={() => onClickLink(onClickSearch)}>
                      Search
                   </NavLink>
                   {user && (
                      <NavLink
                         className="nav-link nav-link--custom"
-                        to="/watchlist">
+                        to="/watchlist"
+                        onClick={() => onClickLink()}>
                         Watch Later
                      </NavLink>
                   )}
                   {user && (
                      <NavLink
                         className="nav-link nav-link--custom"
-                        to="/customers">
+                        to="/customers"
+                        onClick={() => onClickLink()}>
                         Profile
                      </NavLink>
                   )}
@@ -58,12 +75,14 @@ const NavBar = ({ user, onClickSearch, onClickHome }) => {
                      <React.Fragment>
                         <NavLink
                            className="nav-link nav-link--custom"
-                           to="/login">
+                           to="/login"
+                           onClick={() => onClickLink()}>
                            Login
                         </NavLink>
                         <NavLink
                            className="nav-link nav-link--custom"
-                           to="/register">
+                           to="/register"
+                           onClick={() => onClickLink()}>
                            Register
                         </NavLink>
                      </React.Fragment>
@@ -71,7 +90,8 @@ const NavBar = ({ user, onClickSearch, onClickHome }) => {
                   {user && (
                      <NavLink
                         className="nav-link nav-link--custom"
-                        to="/logout">
+                        to="/logout"
+                        onClick={() => onClickLink()}>
                         Logout
                      </NavLink>
                   )}
